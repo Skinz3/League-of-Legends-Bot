6efcea5fb3111e34bc14a740539d6ca72494638e
Skinz3 <oliviamarius35@gmail.com>
2020-04-18 20:26:50 +0200

    Update aram.lua script

--- Binaries/Patterns/aram.lua
+++ Binaries/Patterns/aram.lua
@@ -2,6 +2,12 @@
 ---------
 GAME_PROCESS_NAME = "League of Legends"
 Description = "This pattern play an Aram Game."
+
+Side = false -- Blue = true, Red = false
+
+
+CAST_SPELL_TARGET = {}
+
 --------
 
 function Execute()
@@ -12,62 +18,52 @@ function Execute()
  
     api:waitUntilProcessBounds(GAME_PROCESS_NAME,1030,797)    
 
+    api:wait(200);
+
     api:log("Waiting for game to load.");
 
     api:bringProcessToFront(GAME_PROCESS_NAME);
     api:centerProcess(GAME_PROCESS_NAME)
 
-    api:waitForColor(997,904,"#00D304");
+    WaitForGameToStart();
 
     api:log("We are in game !");
---[[
+
     api:bringProcessToFront(GAME_PROCESS_NAME);
     api:centerProcess(GAME_PROCESS_NAME)
 
-    api:wait(2000);
+    api:wait(1000);
 
-    api:leftClick(1241,920); -- lock camera
+    Side = getSide();
 
-    api:wait(200);
+    if Side == true then
+        CAST_SPELL_TARGET = {1326,320}
+        api:log("We are blue side!");
+    else
+        CAST_SPELL_TARGET = {644,761}
+        api:log("We are red side!");   
+    end
 
-    api:leftClick(826,833); -- upgrade summoner Q
+    LockCamera();
     
-    api:wait(200);
+    UpgradeSummonerSpell1();
+    UpgradeSummonerSpell2();
+    UpgradeSummonerSpell3();
 
-    api:leftClick(875,833); -- upgrade summoner Z
+    OpenShop();
+    BuyItem1();
+    BuyItem2();
     
-    api:wait(200);
-
-    api:leftClick(917,833); -- upgrade summoner E
-
-    api:wait(10000);
-
-    api:rightClick(1351,860); --]]
-
+    api:talk("salam les roya");
 
     while api:isProcessOpen(GAME_PROCESS_NAME) do
-        api:keyDown("F2");
 
-        api:rightClick(886,521);
+        MoveToAlly2();
 
-        api:moveMouse(1026,475);
-        api:pressKey("D1");
-        api:wait(200);
+        CastSpell1();
 
-        api:moveMouse(1026,475);
-        api:pressKey("D2");
-        api:wait(1000);
+        MoveToAlly2();
 
-        api:rightClick(886,521);
-
-        api:moveMouse(1026,475);
-        api:pressKey("D3");
-        api:wait(1000);
-
-        api:rightClick(886,521);
-        
-        api:moveMouse(1026,475);
-        api:pressKey("D4");
         api:wait(1000);
     end
 
@@ -76,4 +72,58 @@ function Execute()
 
 end
 
+function getSide()
+    return api:getColor(1350,856) == "#2E7F98";
+end
 
+function UpgradeSummonerSpell1()
+    api:leftClick(826,833); 
+    api:wait(200);
+end
+
+function UpgradeSummonerSpell2()
+    api:leftClick(875,833);
+    api:wait(200);
+end
+
+function UpgradeSummonerSpell3()
+    api:leftClick(917,833); 
+    api:wait(200);
+end
+
+function WaitForGameToStart()
+    api:waitForColor(997,904,"#00D304");
+end
+
+function LockCamera()
+    api:leftClick(1241,920); 
+    api:wait(200);
+end
+
+function CastSpell1(x,y)
+    api:moveMouse(CAST_SPELL_TARGET[1],CAST_SPELL_TARGET[2]);
+    api:pressKey("D1");
+    api:wait(200);
+end
+
+function MoveToAlly2()
+    api:keyDown("F2");
+    api:rightClick(886,521);
+    api:wait(200);
+end
+
+
+function OpenShop()
+    api:pressKey("P");
+    api:wait(200);
+end
+
+function BuyItem1()
+    api:rightClick(577,337);
+    api:wait(200);
+end
+
+function BuyItem2()
+    api:rightClick(782,336);
+    api:wait(200);
+end
