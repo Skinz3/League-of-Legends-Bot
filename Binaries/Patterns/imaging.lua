ae93023f641d6710c24e7b5fa1e9c4d0edcc38fd
Skinz3 <oliviamarius35@gmail.com>
2020-04-20 17:29:21 +0200

    Now automatically builds to /Binaries/

--- Binaries/Patterns/imaging.lua
+++ Binaries/Patterns/imaging.lua
@@ -1,6 +1,4 @@
 
- -- This pattern script start an Aram Game.
-
 ---------
 CLIENT_PROCESS_NAME = "LeagueClientUX"
 GAME_PROCESS_NAME = "League of Legends"
@@ -9,18 +7,12 @@ Description = "This pattern script finds an image, and clicks it."
 
 function Execute()
 
-
-	while true do
-		game:health();
-	end
-	
-	
-    --api:log("Waiting for test image...");
-	--api:waitForImage( "test" );
-	--api:log("Image found!");
-	--api:wait(2000);
-	--api:leftClickImage( "test" );
-	--api:log("Image clicked!");
+    win:log("Waiting for test image...");
+	win:waitForImage("test.png");
+	win:log("Image found!");
+	win:wait(2000);
+	win:leftClickImage("test.png");
+	win:log("Image clicked!");
 	
 end
 
