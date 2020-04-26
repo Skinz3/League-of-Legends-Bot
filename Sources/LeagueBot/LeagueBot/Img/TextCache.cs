a03124f48073733a196bfc2ef745c678c463e5a4
glenndilen <glenn@glenndilen.co.uk>
2020-04-26 21:30:18 +0100

    Added Text cache

---
+++ Sources/LeagueBot/LeagueBot/Img/TextCache.cs
@@ -0,0 +1,33 @@
+﻿using LeagueBot.IO;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeagueBot.Img
+{
+    public class TextCache
+    {
+    
+        public const int STEP = 250;
+
+        private static Dictionary<string, int[]> ImagePixels = new Dictionary<string, int[]>();
+        private static Dictionary<string, int>   ImageHeigth = new Dictionary<string, int>();
+        private static Dictionary<string, int>   ImageWidth = new Dictionary<string, int>();
+
+      
+  
+
+
+
+      
+        
+    }
+}
