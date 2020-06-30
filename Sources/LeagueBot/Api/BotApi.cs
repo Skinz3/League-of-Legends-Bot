77055f2450951f00f3d3662dfaa76df46672291b
kevdah <kevin.dahlen@gmail.com>
2020-06-30 06:37:41 +0200

    API implemented

---
+++ Sources/LeagueBot/Api/BotApi.cs
@@ -0,0 +1,97 @@
+﻿using LeagueBot.ApiHelpers;
+using LeagueBot.IO;
+using LeagueBot.Patterns;
+using LeagueBot.Windows;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeagueBot.Windows.Interop;
+using LeagueBot.Patterns;
+
+namespace LeagueBot.Api
+{
+    public class BotApi : IApi
+    {
+        //public int _outMaxTime = 360; // 6 minutes.
+        //public int _outActualTime = 0;
+
+        public void log(string message)
+        {
+            BotHelper.Log(message);
+        }
+        public void wait(int ms)
+        {
+            BotHelper.Wait(ms);
+        }
+        public void executePattern(string name)
+        {
+            PatternsManager.Execute(name);
+        }
+        public void waitUntilProcessBounds(string processName, int boundsX, int boundsY)
+        {
+            RECT rect = new RECT();
+
+            int width = 0;
+            int height = 0;
+
+            while (width != boundsX && height != boundsY)
+            {
+                var process = Process.GetProcessesByName(processName).FirstOrDefault();
+
+                if (process == null)
+                {
+                    throw new Exception("Process " + process + " cannot be found.");
+                }
+
+                Interop.GetWindowRect(process.MainWindowHandle, out rect);
+
+                width = rect.Right - rect.Left;
+                height = rect.Bottom - rect.Top;
+
+                BotHelper.Wait(1000);
+
+            }
+        }
+        public bool isProcessOpen(string processName)
+        {
+            return Process.GetProcessesByName(processName).Length > 0;
+        }
+        public void centerProcess(string processName)
+        {
+            if (!Interop.CenterProcessWindow(processName))
+            {
+                Logger.Write("Unable to center process: " + processName, MessageState.WARNING);
+            }
+        }
+        public void bringProcessToFront(string processName)
+        {
+            if (!Interop.BringWindowToFront(processName))
+            {
+                Logger.Write("Unable to bring process to front: " + processName, MessageState.WARNING);
+            }
+        }
+        public void waitProcessOpen(string processName)
+        {
+            while (!Interop.IsProcessOpen(processName))
+            {
+                BotHelper.Wait(1000);
+
+                /*if(processName == "League of Legends") { 
+                    _outActualTime++;
+
+                    if(_outActualTime == _outMaxTime)
+                    {
+                        Logger.Write("Someone picked your champ... ", MessageState.WARNING);
+                    }
+                }*/
+            }
+        }
+        public void inputWords(string words, int keyDelay = 50, int delay = 100)
+        {
+            InputHelper.InputWords(words, keyDelay, delay);
+        }
+    }
+}
---
+++ Sources/LeagueBot/Program.cs
@@ -0,0 +1,75 @@
+﻿using LeagueBot.DesignPattern;
+using LeagueBot.Game;
+using LeagueBot.Game.Enums;
+using LeagueBot.Image;
+using LeagueBot.IO;
+using LeagueBot.Patterns;
+using LeagueBot.Texts;
+using LeagueBot.Windows;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static LeagueBot.Windows.Interop;
+
+namespace LeagueBot
+{
+    /* 
+     * The user input settings (game mode, champion , champion behaviour)
+     * -> We create SessionParameters.cs that we pass to each scripts.
+     * Or
+     * 
+     * The user input settings (game mode, champion , champion behaviour)
+     * -> We create Session.cs that call scripts.
+     */
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Logger.OnStartup();
+
+            StartupManager.Initialize(Assembly.GetExecutingAssembly());
+
+            checkChampList();
+
+            if (args.Length == 0)
+                HandleCommand(string.Empty);
+            else
+                HandleCommand(args[0]);
+            
+            Console.Read();
+        }
+
+        static void HandleCommand(string restart)
+        {
+            string line;
+
+            if (restart == string.Empty)
+            {
+                Logger.Write("Enter a pattern filename, type 'help' for help.", MessageState.INFO);
+                line = Console.ReadLine();
+            } else { line = restart; }
+            
+
+            PatternsManager.Execute(line);
+
+            HandleCommand(string.Empty);
+        }
+
+        private static void checkChampList()
+        {
+            string path = Directory.GetCurrentDirectory() + "\\champlist.txt";
+            if (!File.Exists(path))
+            {
+                Logger.WriteColor1("champlist.txt not found. Creating file...\nPlease fill the list that has been generated with champion names.\nNOTE: One champion per line.");
+                File.Create(path);
+            }
+        }
+    }
+}
