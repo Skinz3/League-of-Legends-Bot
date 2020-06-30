f596302746a98dbba0ed5db0587d36c560a2fa5a
kevdah <kevin.dahlen@gmail.com>
2020-06-30 06:36:07 +0200

    Improved game logic, LCU API implemented

---
+++ Binaries/Patterns/Test.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using LeagueBot.Patterns;
+
+namespace LeagueBot
+{
+    public class Test : PatternScript
+    {
+
+        public override void Execute()
+        {
+            bot.wait(5000);
+            bot.log("Match ended.");
+
+            bot.waitProcessOpen(CLIENT_PROCESS_NAME);
+
+            bot.bringProcessToFront(CLIENT_PROCESS_NAME);
+            bot.centerProcess(CLIENT_PROCESS_NAME);
+
+            bot.wait(5000);
+
+            client.skipHonor();
+
+            bot.wait(4000);
+
+            /*if (client.levelUp())
+            {
+                bot.log("level up!");
+                client.skipLevelRewards();
+            }*/
+
+            client.skipLevelRewards();
+
+            bot.wait(4000);
+
+            client.skipLevelRewards();
+
+            bot.wait(4000);
+
+            client.skipLevelRewards();
+
+            /*if (client.questCompleted())
+            {
+                bot.log("quest completed!");
+                client.skipLevelRewards();
+            }
+            bot.wait(4000);
+            if (client.questCompleted())
+            {
+                bot.log("quest completed!");
+                client.skipLevelRewards();
+            }
+            bot.wait(4000);
+            if (client.questCompleted())
+            {
+                bot.log("quest completed!");
+                client.skipLevelRewards();
+            }*/
+
+            bot.wait(4000);
+
+            client.closeGameRecap();
+
+            bot.wait(4000);
+        }
+    }
+}
