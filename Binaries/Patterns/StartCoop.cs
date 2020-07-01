b3a84a24ef3449d015088384392b3f338d231aed
kevdah <kevin.dahlen@gmail.com>
2020-07-01 16:50:15 +0200

    API fix

--- Binaries/Patterns/StartCoop.cs
+++ Binaries/Patterns/StartCoop.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -17,51 +17,32 @@ namespace LeagueBot
 
         public override void Execute()
         {
-            bot.log("Waiting for league client process... Ensure League client window size is 1600x900");
+            bot.log("Waiting for league client process...");
             bot.waitProcessOpen(CLIENT_PROCESS_NAME);
-            
             bot.bringProcessToFront(CLIENT_PROCESS_NAME);
-            bot.waitUntilProcessBounds(CLIENT_PROCESS_NAME, 1600, 900);
             bot.centerProcess(CLIENT_PROCESS_NAME);
-
-            bot.wait(5000);
-
-            bot.bringProcessToFront(CLIENT_PROCESS_NAME);
-            bot.waitUntilProcessBounds(CLIENT_PROCESS_NAME, 1600, 900);
-            bot.centerProcess(CLIENT_PROCESS_NAME);
-
-            //REDO
 
             bot.log("Client ready.");
             bot.wait(2000);
-            client.clickPlayButton();
-            bot.wait(2000);
-            client.clickCoopvsIAText();
-            bot.wait(2000);
+            client.createLobby(MODE);
 
-            if (MODE == "intermediate")
+            bot.log("Attempting to search for game...");
+
+            client.startQueue();
+            bool restartneeded = false;
+
+            while (client.leaverbuster())
             {
-                client.clickIntermediateText();
+                restartneeded = true;
+                bot.log("Leaverbuster detected. Waiting 30 seconds.");
+                bot.wait(30000);
             }
-            else if (MODE == "intro")
+
+            if (restartneeded) { client.startQueue(); }
+
+            while (!client.inChampSelect())
             {
-                client.clickIntroText();
-            }
-
-
-            bot.wait(2000);
-            client.clickConfirmButton();
-
-            bot.wait(5000);
-
-            client.clickFindMatchButton();
-
-            bot.log("Finding match...");
-
-            while (client.mustSelectChamp() == false)
-            {
-				client.clickFindMatchButton();
-                client.acceptMatch();
+                client.acceptQueue();
                 bot.wait(3000);
             }
 
@@ -73,56 +54,15 @@ namespace LeagueBot
             {
                 foreach (string champ in champs)
                 {
-                    pickChamp(champ);
-                    client.clearChampSearch();
-                    bot.wait(150);
-                    client.clearChampSearch();
+                    bot.log("Attempting to pick "+champ);
+                    client.pickChampionByName(champ);
                 }
             } else
             {
-                pickChamp(SELECTED_CHAMPION);
+                client.pickChampionByName(SELECTED_CHAMPION);
             }
 
-
-           /* RandomTextSender = new Random();
-            switch (RandomTextSender.Next(5))
-            {
-                case 1:
-                    game.chat.talkInChampSelect("hi guys");
-                    game.chat.talkInChampSelect("supporting bot");
-                    break;
-                case 2:
-                    game.chat.talkInChampSelect("gl boys dont flame");
-                    break;
-                case 3:
-                    game.chat.talkInChampSelect("sup");
-                    break;
-                case 4:
-                    game.chat.talkInChampSelect("bot");
-                    break;
-                case 5:
-                    game.chat.talkInChampSelect("support");
-                    break;
-                default:
-                    game.chat.talkInChampSelect("Hi guys");
-                    game.chat.talkInChampSelect("Going botlane");
-                    break;
-            }
-            //champion not selected?
-*/
             bot.executePattern("Coop");
-        }
-
-        void pickChamp(string champ)
-        {
-            client.clickChampSearch();
-            bot.wait(500);
-            bot.inputWords(champ);
-            bot.wait(300);
-            client.selectFirstChampion();
-            bot.wait(500);
-            client.lockChampion();
-            bot.wait(1000);
         }
     }
 }
