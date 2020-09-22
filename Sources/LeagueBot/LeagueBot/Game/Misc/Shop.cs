620ef9dabce2d217325c86ef81bf9a81f0b2c190
Skinz3 <oliviamarius35@gmail.com>
2020-09-22 03:15:51 +0200

    Refactoring

--- Sources/LeagueBot/LeagueBot/Game/Misc/Shop.cs
+++ Sources/LeagueBot/LeagueBot/Game/Misc/Shop.cs
@@ -30,10 +30,6 @@ namespace LeagueBot.Game.Misc
             Opened = !Opened;
         }
 
-        public int getPlayerGold()
-        {
-            return TextHelper.GetTextFromImage(767, 828, 118, 34);
-        }
         public void searchItem(string name)
         {
             InputHelper.KeyDown("LControlKey");
@@ -55,51 +51,6 @@ namespace LeagueBot.Game.Misc
             BotHelper.InputIdle();
 
             InputHelper.PressKey("Enter");
-
-            BotHelper.InputIdle();
-        }
-
-        public void buyItem(int indice)
-        {
-            Point coords = new Point(0, 0);
-
-            switch (indice)
-            {
-                case 1:
-                    coords = new Point(577, 337);
-                    break;
-                case 2:
-                    coords = new Point(782, 336);
-                    break;
-                case 3:
-                    coords = new Point(913, 345);
-                    break;
-                case 4:
-                    coords = new Point(575, 455);
-                    break;
-                case 5:
-                    coords = new Point(534, 518);
-                    break;
-                case 6:
-                    coords = new Point(756, 516);
-                    break;
-                case 7:
-                    coords = new Point(907, 513);
-                    break;
-                case 8:
-                    coords = new Point(532, 630);
-                    break;
-                case 9:
-                    coords = new Point(937, 565);
-                    break;
-                case 10:
-                    coords = new Point(565, 681);
-                    break;
-                default:
-                    Logger.Write("Unknown item indice " + indice + ". Skipping", MessageState.WARNING);
-                    return;
-            }
-            InputHelper.RightClick(coords.X, coords.Y);
 
             BotHelper.InputIdle();
         }
