41bae5732e7c692e37989e8053dd9cdbd0cb6ae5
Buiko32 <50544394+Buiko32@users.noreply.github.com>
2020-11-29 18:58:08 +0100

    Fix

--- Binaries/Patterns/Coop.cs
+++ Binaries/Patterns/Coop.cs
@@ -131,7 +131,7 @@ namespace LeagueBot
                     if (game.player.getManaPercent() == 1)
                     {
                         OnSpawnJoin();
-                        isRecalling = false;
+                
                     }
          
 
