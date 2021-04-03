4aa4abb493dc14f7c5c94b1be69b6be1e28a9a94
Kacper Bąk <56700396+53jk1@users.noreply.github.com>
2021-04-03 17:09:08 +0200

    Update StartCoop.cs

--- Binaries/Patterns/StartCoop.cs
+++ Binaries/Patterns/StartCoop.cs
@@ -1,273 +1,162 @@
-
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using LeagueBot.Patterns;
 using LeagueBot.ApiHelpers;
+using LeagueBot.Game.Enums;
 
 namespace LeagueBot
 {
     public class StartCoop : PatternScript
     {
-        private const string MODE = "intermediate";
-        
-        private Random RandomTextSender;
-        
-        private Random RandomChampion;
+        private static ChampionEnum[] Champions = new ChampionEnum[]
+        {
+			ChampionEnum.Veigar,
+            ChampionEnum.Caitlyn,
+            ChampionEnum.MissFortune,
+            
+        };
 
+        private static QueueEnum QueueType = QueueEnum.BotIntro;
+
+        
         public override void Execute()
         {
-            bot.log("Waiting for league client process... Ensure League client window size is 1600x900");
-            bot.waitProcessOpen(CLIENT_PROCESS_NAME);
+            client.openClient();
+
+            bot.log("Waiting for league client process...");
+
+            bot.waitProcessOpen(Constants.ClientProcessName);
+
+            bot.bringProcessToFront(Constants.ClientProcessName);
             
-            bot.bringProcessToFront(CLIENT_PROCESS_NAME);
-            bot.waitUntilProcessBounds(CLIENT_PROCESS_NAME, 1600, 900);
-            bot.centerProcess(CLIENT_PROCESS_NAME);
+            bot.centerProcess(Constants.ClientProcessName);   
 
-            bot.wait(5000);
+            client.initialize(); // read current league process informations
 
-            bot.bringProcessToFront(CLIENT_PROCESS_NAME);
-            bot.waitUntilProcessBounds(CLIENT_PROCESS_NAME, 1600, 900);
-            bot.centerProcess(CLIENT_PROCESS_NAME);
+            bot.log("Waiting for league client to be ready...");
 
-            //REDO
+            client.waitClientReady(); 
 
-            bot.log("Client ready.");
-            bot.wait(2000);
-            client.clickPlayButton();
-            bot.wait(2000);
-            client.clickCoopvsIAText();
-            bot.wait(2000);
-
-            if (MODE == "intermediate")
+            while (!client.loadSummoner())
             {
-                client.clickIntermediateText();
-            }
-            else if (MODE == "intro")
-            {
-                client.clickIntroText();
+                bot.warn("Unable to load summoner. Retrying in 10 seconds.");
+                bot.wait(1000);
             }
 
+            bot.log("Summoner loaded "+ client.summoner.displayName);
 
-            bot.wait(2000);
-            client.clickConfirmButton();
+            bot.wait(3000);
 
-            bot.wait(5000);
+            client.createLobby(QueueType);
 
-            client.clickFindMatchButton();
+           
 
-            bot.log("Finding match...");
+            ProcessMatch();
+        
 
-            while (client.mustSelectChamp() == false)
+        }
+        private void ProcessMatch()
+        {
+            bot.log("Searching match...");
+            
+            bot.wait(3000);
+
+            SearchMatchResult result = client.searchMatch();
+
+            while (result != SearchMatchResult.Ok)
+            {   
+                switch (result)
+                {
+                    case SearchMatchResult.GatekeeperRestricted:
+                       bot.warn("Cannot search match. Queue dodge timer. Retrying in 20 seconds.");
+                       bot.wait(1000 * 20);
+                       break;
+                    case SearchMatchResult.QueueNotEnabled:
+                       client.createLobby(QueueType);
+                       bot.warn("Cannot search match. Creating lobby...");
+                       bot.wait(1000 * 10);
+                       break;
+                    case SearchMatchResult.InvalidLobby:
+                       bot.warn("Cannot search match. Client not ready. Retrying in 10 seconds.");
+                        bot.wait(1000 * 10);
+                       break;
+                }
+
+               
+
+                result = client.searchMatch();
+            }
+
+            bool isMatchFound = false;
+
+            while (!isMatchFound)
+            {
+                isMatchFound = client.isMatchFound();
+                bot.wait(1000);
+
+            }
+
+            while (client.getGameflowPhase() != GameflowPhaseEnum.ChampSelect)
             {
                 client.acceptMatch();
-                bot.wait(3000);
+                bot.wait(1000);
             }
 
-            bot.log("Match found");
 
-            client.clickChampSearch();
-            bot.wait(2000);
+            bot.log("Match founded.");
 
+            bot.wait(5000);
 
-            //here you have to give some samples of champions you own
-            RandomChampion = new Random();
-            switch(RandomChampion.Next(39))
+            bool picked = false;
+
+            int championIndex = 0;
+
+            while (!picked)
             {
-                case 1:
-                    bot.inputWords("Taric");
-                    break;
+                if (championIndex > Champions.Length - 1)
+                {
+                    bot.warn("Unable to continue. No more champions to pick");
+                    return;
+                }
+                ChampionPickResult pickResult = client.pickChampion(Champions[championIndex]);
 
-                case 2:
-                    bot.inputWords("Volibear");
-                    break;
+                switch (pickResult)
+                {
+                    case ChampionPickResult.Ok:
+                        bot.log(Champions[championIndex] + " picked successfully");
+                        picked = true;
+                        break;
+                    case ChampionPickResult.ChampionNotOwned:
+                        bot.warn("Error the request champion is not owned.");
+                        break;
+                    case ChampionPickResult.ChampionPicked:
+                        bot.warn("Someone already pick your champion");
+                        break;
+                    default:
+                        break;
+                }
 
-                case 3:
-                    bot.inputWords("Urgot");
-                    break;
+                championIndex++;
 
-                case 4:
-                    bot.inputWords("Sona");
-                    break;
-
-                case 5:
-                    bot.inputWords("Garen");
-                    break;
-
-                case 6:
-                    bot.inputWords("Amumu");
-                    break;
-
-                case 7:
-                    bot.inputWords("Vi");
-                    break;
-
-                case 8:
-                    bot.inputWords("Lux");
-                    break;
-
-                case 9:
-                    bot.inputWords("Caitlyn");
-                    break;
-
-                case 10:
-                    bot.inputWords("Miss Fortune");
-                    break;
-
-                case 11:
-                    bot.inputWords("Tristana");
-                    break;
-
-                case 12:
-                    bot.inputWords("Ashe");
-                    break;
-
-                case 13:
-                    bot.inputWords("Soraka");
-                    break;
-
-                case 14:
-                    bot.inputWords("Kayle");
-                    break;
-
-                case 15:
-                    bot.inputWords("Janna");
-                    break;
-
-                case 16:
-                    bot.inputWords("Udyr");
-                    break;
-
-                case 17:
-                    bot.inputWords("Annie");
-                    break;
-
-                case 18:
-                    bot.inputWords("Master Yi");
-                    break;
-
-                case 19:
-                    bot.inputWords("Cassiopeia");
-                    break;
-
-                case 20:
-                    bot.inputWords("Sivir");
-                    break;
-
-                case 21:
-                    bot.inputWords("Brand");
-                    break;
-                
-                case 22:
-                    bot.inputWords("Ahri");
-                    break;
-
-                case 23:
-                    bot.inputWords("Darius");
-                    break;
-
-                case 24:
-                    bot.inputWords("Nidalee");
-                    break;
-
-                case 25:
-                    bot.inputWords("Warwick");
-                    break;
-
-                case 26:
-                    bot.inputWords("Shaco");
-                    break;
-
-                case 27:
-                    bot.inputWords("Nunu");
-                    break;
-
-                case 28:
-                    bot.inputWords("Rammus");
-                    break;
-
-                case 29:
-                    bot.inputWords("Lucian");
-                    break;
-
-                case 30:
-                    bot.inputWords("Xin Zhao");
-                    break;
-
-                case 31:
-                    bot.inputWords("Mordekaiser");
-                    break;
-
-                case 32:
-                    bot.inputWords("Dr. Mundo");
-                    break;
-
-                case 33:
-                    bot.inputWords("Skarner");
-                    break;
-
-                case 34:
-                    bot.inputWords("Poppy");
-                    break;
-
-                case 35:
-                    bot.inputWords("Zilean");
-                    break;
-                
-                case 36:
-                    bot.inputWords("Singed");
-                    break;
-                
-                case 37:
-                    bot.inputWords("Ryze");
-                    break;
-
-                case 38:
-                    bot.inputWords("Talon");
-                    break;
-
-                case 39:
-                    bot.inputWords("Fiddlesticks");
-                    break;
-
-                
+                bot.wait(1000);
             }
 
-            bot.wait(2000);
+            bot.log("Waiting....");
 
-            client.selectFirstChampion();
+            GameflowPhaseEnum currentPhase = client.getGameflowPhase();
 
-            bot.wait(2000);
-
-            client.lockChampion();
-
-            bot.wait(1500);
-
-            RandomTextSender = new Random();
-            switch (RandomTextSender.Next(5))
+            while (currentPhase != GameflowPhaseEnum.InProgress)
             {
-                case 1:
-                    game.chat.talkInChampSelect("hi guys");
-                    game.chat.talkInChampSelect("supporting bot");
-                    break;
-                case 2:
-                    game.chat.talkInChampSelect("gl boys dont flame");
-                    break;
-                case 3:
-                    game.chat.talkInChampSelect("sup");
-                    break;
-                case 4:
-                    game.chat.talkInChampSelect("bot");
-                    break;
-                case 5:
-                    game.chat.talkInChampSelect("support");
-                    break;
-                default:
-                    game.chat.talkInChampSelect("Hi guys");
-                    game.chat.talkInChampSelect("Going botlane");
-                    break;
+                if (currentPhase != GameflowPhaseEnum.ChampSelect)
+                {
+                    bot.log("Game was dodged, finding match....");
+                    ProcessMatch();
+                    return;
+                }
+                bot.wait(1000);
+                currentPhase = client.getGameflowPhase();
             }
-            //champion not selected?
 
             bot.executePattern("Coop");
         }
